68b4576b61435c504641ebd13a17aa5969af8e83
mountain <Fiveoat@gmail.com>
2020-11-05 00:04:26 -0700

    akdfj

--- myuseradd.sh
+++ myuseradd.sh
@@ -49,7 +49,7 @@ function delete_user () {
 # 
 function add_user () {
 	echo "add_user"
-	NUM_RESULTS = getent passwd | grep "$1" | wc -l
+	NUM_RESULTS=`getent passwd | grep "$1" | wc -l`
 	if [ $NUM_RESULTS < 1 ]
 	then 
 		useradd -m "$1" -s "$3" && echo "$1":"$2" | chpasswd && echo "$1" "(" "$2" ")with " "$3" "is added"
