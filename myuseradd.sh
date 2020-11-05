0121ac63f69503063b8826aea3eeea49abd86007
mountain <Fiveoat@gmail.com>
2020-11-04 23:31:40 -0700

    alksdjf

--- myuseradd.sh
+++ myuseradd.sh
@@ -43,9 +43,11 @@ function delete_user () {
 # 
 function add_user () {
 	echo "add_user"
-	echo "$1" "$2" "$3"
-	adduser "$2" && echo "$2:$3" | chpasswd
-	echo "$3" | passwd "$2"
+	echo "$1" 
+	echo"$2" 
+	echo "$3"
+	adduser "$1" && echo "$1:$2" | chpasswd
+	echo "$2" | passwd "$1"
 }
 
 
